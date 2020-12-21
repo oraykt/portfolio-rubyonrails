4c65119daa5cb3048c041f3cf9fbdd0658d9b7e9
Oray KURT <oraykt@gmail.com>
2020-12-21 15:16:09 +0100

    Integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,29 @@
-class PortfoliosController < ApplicationController
+# frozen_string_literal: true
 
+class PortfoliosController < ApplicationController
   def index
     @portfolio_items = Portfolio.all
+  end
+
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio saved successfully' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
   end
 end
