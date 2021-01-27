eaf58c7c8b50c54360aa6a5a20411061d6453ae6
Oray KURT <oraykt@gmail.com>
2021-01-27 12:57:03 +0100

    Implemented custom action > toggle_Status

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class BlogsController < ApplicationController
-  before_action :set_blog, only: %i[show edit update destroy]
+  before_action :set_blog, only: %i[show edit update destroy, toggle_status]
 
   # GET /blogs
   # GET /blogs.json
@@ -54,6 +54,15 @@ class BlogsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
     end
+  end
+
+  def toggle_status
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!
+    end
+    redirect_to blogs_url, notice: 'Post status has been updated.'
   end
 
   private
