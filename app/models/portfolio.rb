6bfb77b0db04f04da82545aab06490d30f208605
Oray KURT <oraykt@gmail.com>
2021-01-31 15:36:35 +0100

    Implemented image defaults for portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,15 +1,21 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
 
+  # Portfolio.custom_query_1
   def self.custom_query_1
     where(subtitle: 'React')
   end
 
+  # Portfolio.custom_query_2
   scope :custom_query_2, ->{
     where(subtitle: 'Ruby on Rails')
   }
 
-  # Custom queries usage
-  # Portfolio.custom_query_1
-  # Portfolio.custom_query_2
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "http://via.placeholder.com/600x400"
+    self.thumb_image ||= "http://via.placeholder.com/300x200"
+  end
+
 end
