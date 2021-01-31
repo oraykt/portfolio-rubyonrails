477b42a10b318b94d7d1ac0b30ea7f61f4388453
Oray KURT <oraykt@gmail.com>
2021-01-31 15:10:44 +0100

    Implemented custom queries for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,15 @@
 class Portfolio < ApplicationRecord
-
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  def self.custom_query_1
+    where(subtitle: 'React')
+  end
+
+  scope :custom_query_2, ->{
+    where(subtitle: 'Ruby on Rails')
+  }
+
+  # Custom queries usage
+  # Portfolio.custom_query_1
+  # Portfolio.custom_query_2
 end
