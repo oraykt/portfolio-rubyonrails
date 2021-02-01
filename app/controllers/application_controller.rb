59a75b0e7feae9328ddcd646975d8878539a9092
Oray KURT <oraykt@gmail.com>
2021-02-01 13:10:57 +0100

    Implemented controller concern for custom devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::Base
-
-  before_action :configure_permitted_parameters,
-                if: :devise_controller?
-
-
-  def configure_permitted_parameters
-    # devise knows sign_up and account_update keywords on usage.
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
-  end
+  include DeviseWhitelist
 end
---
+++ app/controllers/concerns/devise_whitelist.rb
@@ -0,0 +1,15 @@
+module DeviseWhitelist
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :configure_permitted_parameters,
+                  if: :devise_controller?
+  end
+
+
+  def configure_permitted_parameters
+    # devise knows sign_up and account_update keywords on usage.
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
+  end
+end
