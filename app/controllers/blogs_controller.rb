004252d9ac2f2bccc5f8d7d7994a46c9a7a201cd
Oray KURT <oraykt@gmail.com>
2021-02-05 08:55:24 +0100

    Pagination

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,7 +8,7 @@ class BlogsController < ApplicationController
 
   # GET /blogs
   def index
-    @blogs = Blog.all
+    @blogs = Blog.page(params[:page]).per(5)
     @page_title = 'Oray Kurt | Blogs'
   end
 
