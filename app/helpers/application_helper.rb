5118a6857fa211b0b8a9f2b1cb3e13930ce0f873
Oray KURT <oraykt@gmail.com>
2021-02-06 23:46:43 +0100

    Notifications refactored

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -56,4 +56,13 @@ module ApplicationHelper
   def active? path
     "active" if current_page? path
   end
+
+  def alerts
+    alert = (flash[:notice] || flash[:error] || flash[:alert])
+    if alert
+      js add_gritter(alert)
+    end
+  end
+
+
 end
