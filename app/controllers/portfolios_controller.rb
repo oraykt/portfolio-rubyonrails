26657748fcf6ed028320cc55bce5824c322f8528
Oray KURT <oraykt@gmail.com>
2021-02-06 10:58:56 +0100

    Authorization rule for drag-drop feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,19 +1,19 @@
 class PortfoliosController < ApplicationController
   before_action :set_portfolio, only: %i[show edit update destroy toggle_status]
   layout 'portfolio'
-  access all: [ :show, :index ],
-         user: { except: [ :new, :create, :update, :edit, :destroy ]},
+  access all: %i[show index],
+         user: { except: %i[new create update edit destroy sort] },
          site_admin: :all
 
   # GET /portfolios
   def index
-    @portfolios= Portfolio.by_position
+    @portfolios = Portfolio.by_position
     @page_title = 'Oray Kurt | Portfolios'
   end
 
   # PUT /portfolios/sort
   def sort
-    params[:order].each do |key, value|
+    params[:order].each do |_key, value|
       Portfolio.find(value[:id]).update(position: value[:position])
     end
 
